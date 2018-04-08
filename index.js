996f20d610efdcc4f37fed2f52a0587e25cee2f9
Swastik Shetty <swshetty@deloitte.com>
2018-04-08 20:16:04 +0530

    - added check for /start command

--- index.js
+++ index.js
@@ -23,11 +23,9 @@ app.post('/new-message', function(req, res) {
   const {message} = req.body
   console.log("req == "+JSON.stringify(message));
 
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-  if (!message || message.text.toLowerCase().indexOf('marco') <0) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    return res.end()
-  }
+  parseMessage(message, res);
+
+  
 
   // If we've gotten this far, it means that we have received a message containing the word "marco".
   // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
@@ -35,6 +33,27 @@ app.post('/new-message', function(req, res) {
   sendMessage(message.chat.id, 'Polo!!', res);
 
 });
+
+function parseMessage(message, resObj){
+  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
+  // if (!message || message.text.toLowerCase().indexOf('marco') <0) {
+  //   // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
+  //   return resObj.end()
+  // }
+
+  if(!message){
+    return resObj.end();
+  }
+
+  if(message.entities && message.entities[0].type == "bot_command"){
+    if(message.text && message.text.toLowerCase() == "/start"){
+      sendMessage(message.chat.id, "Great, good choice. I can help with following items today", resObj);
+    }
+  }
+
+  sendMessage(message.chat.id, "Sorry, i dont understand that message", resObj);
+
+};
 
 function sendMessage(chatID, responseMsg, resObj){
   axios.post('https://api.telegram.org/bot553303104:AAEVsFhPt0fa8Yw2jJIEcvOOMd7RAmqWjaE/sendMessage', {
