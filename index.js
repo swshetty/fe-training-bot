096d1e9009a4fb42070c58efb1aa1b46c7fdc6d2
Swastik Shetty <swshetty@deloitte.com>
2018-04-26 17:32:12 +0530

    - html formatting

--- index.js
+++ index.js
@@ -133,11 +133,8 @@ function showCertificates(username, chatId, respObj){
       console.log("Found the following records");
       console.log(docs);
 
-      let chatMessage = "<pre>"+
-                          "<b>Previously Uploaded Certificates for : "+username+"</b>"+
-                          "<i>"+docs[0].certificates.toString()+"</i>"+
-                        "</pre>";
-
+      let chatMessage = "<pre><b>Previously Uploaded Certificates for : "+username+"</b>"+
+                          "<i>"+docs[0].certificates.toString()+"</i></pre>";
 
       sendMessage(chatId, chatMessage, respObj, undefined,'HTML');
 
